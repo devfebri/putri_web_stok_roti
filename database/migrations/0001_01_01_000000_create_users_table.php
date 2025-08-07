31cd2dbc161ff81860b532d1887362e41bc78273
Febri <dev.pebri@gmail.com>
2025-08-07 09:23:11 +0700

    fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
                 $table->string('email')->nullable();
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
-                $table->enum('role', ['pimpinan', 'kepalabakery', 'kepalatokokios', 'admin', 'frontliner'])->default('kepalatoko');
+                $table->enum('role', ['pimpinan', 'kepalabakery', 'kepalatokokios', 'admin', 'frontliner'])->default('kepalatokokios');
                 $table->string('phone')->nullable();
                 $table->rememberToken();
                 $table->timestamps();
